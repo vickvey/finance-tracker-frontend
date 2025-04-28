4e8a70623193e92b501082dc4e9d0c3512bfff62
vickvey <vickvey0303@gmail.com>
2025-04-28 12:09:04 +0530

    improved sidebar and top header in user dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import Sidebar from '@/components/sidebar';
+import { BellIcon, ChevronDown, Search } from 'lucide-react';
 
 type DashboardLayoutProps = {
   children: React.ReactElement;
@@ -12,11 +13,42 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className='flex'>
       {/* SIDEBAR */}
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <Sidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        key={0}
+      />
 
       {/* MAIN CONTENT */}
       <main className='w-full bg-[#f8f8f8] text-black/80'>
-        <header className='bg-[#ffffff] h-20 border-b-[1px] border-black/20 shadow-2xs'></header>
+        <header className='bg-[#ffffff] flex justify-between items-center h-18 border-b-[1px] py-3 px-8 border-black/10 shadow-2xs'>
+          <div className='flex flex-col'>
+            <h1>Dashboard</h1>
+            <p className='text-sm text-black/60'>
+              Financial report for{' '}
+              <span className='font-bold'>Feb 01, 2024 - Feb 28, 2024</span>
+            </p>
+          </div>
+
+          <div className='flex items-center gap-4'>
+            <div className='flex gap-2 border-r-[1px] border-black/10 px-2'>
+              <div className='cursor-pointer text-black/60 hover:text-black transition-all duration-100'>
+                <Search />
+              </div>
+              <div className='cursor-pointer text-black/60 hover:text-black transition-all duration-100'>
+                <BellIcon />
+              </div>
+            </div>
+
+            {/* PROFILE ICON AND DROP DOWN BUTTON */}
+            <div className='flex items-center'>
+              <div className='h-10 w-10 rounded-full bg-gray-400'></div>
+              <div className='text-black/60 hover:text-black cursor-pointer flex items-center gap-1'>
+                <ChevronDown />
+              </div>
+            </div>
+          </div>
+        </header>
         {children}
         <footer></footer>
       </main>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,29 +1,48 @@
 import {
+  ArrowDownUp,
   ArrowLeftRight,
   Banknote,
   ChevronsLeftRight,
   Coins,
   CreditCard,
+  HandCoins,
+  HelpCircle,
   LayoutDashboard,
   PiggyBank,
+  Settings,
 } from 'lucide-react';
-import SidebarNav from './sidebar-nav';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const mainMenuItems = [
+  { label: 'Dashboard', href: '/user', icon: <LayoutDashboard /> },
+  { label: 'Cards', href: '#', icon: <CreditCard /> },
+  { label: 'Transactions', href: '#', icon: <ArrowLeftRight /> },
+  { label: 'Cash Flow', href: '#', icon: <Banknote /> },
+  { label: 'Budget', href: '#', icon: <HandCoins /> },
+  { label: 'Goals', href: '#', icon: <PiggyBank /> },
+];
+
+const investmentsMenuItems = [
+  { label: 'Investments', href: '#', icon: <Coins /> },
+  { label: 'Exchanges', href: '#', icon: <ArrowDownUp /> },
+];
+
+const footerItems = [
+  { label: 'Settings', href: '#settings', icon: <Settings /> },
+  { label: 'Help & Support', href: '#help-and-support', icon: <HelpCircle /> },
+];
 
 type SidebarProps = {
   sidebarOpen: boolean;
   setSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const mainMenuNavItems = [
-  { label: 'Dashboard', href: '/user', icon: <LayoutDashboard /> },
-  { label: 'Cards', href: '#', icon: <CreditCard /> },
-  { label: 'Transactions', href: '#', icon: <ArrowLeftRight /> },
-  { label: 'Cash Flow', href: '#', icon: <Banknote /> },
-  { label: 'Budget', href: '#', icon: <Coins /> },
-  { label: 'Goals', href: '#', icon: <PiggyBank /> },
-];
-
 export default function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
+  const [activeNavItem, setActiveNavItem] = useState<string>(
+    mainMenuItems[0].label,
+  );
+
   return (
     <aside
       className={`bg-[#06140f] flex flex-col h-screen ${
@@ -31,7 +50,7 @@ export default function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
       } transition-all duration-200`}
     >
       {/* LOGO, NAME AND SIDEBAR TOGGLE */}
-      <header className='flex items-center justify-between h-20 border-b-[1px] border-white/20 px-4'>
+      <header className='flex items-center justify-between min-h-18 border-b-[1px] border-white/20 px-4'>
         {/* LOGO AND NAME */}
         <div className='flex items-center gap-2'>
           <div className='h-8 w-8 bg-white/80 rounded-md'></div>
@@ -40,7 +59,7 @@ export default function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
               sidebarOpen ? 'block' : 'hidden'
             }`}
           >
-            Weavy
+            FinTrack
           </h1>
         </div>
         {/* SIDEBAR TOGGLE */}
@@ -54,19 +73,133 @@ export default function Sidebar({ sidebarOpen, setSidebarOpen }: SidebarProps) {
         </div>
       </header>
 
-      <section className='flex flex-col bg-green-950/80'>
+      {/* NAV BARS */}
+      <section className='flex flex-col'>
         {/* MAIN MENU NAVBAR */}
-        <SidebarNav
-          sidebarOpen={sidebarOpen}
-          navItems={mainMenuNavItems}
-          key={0}
-        />
+        <section className='px-3 pt-5'>
+          <h1
+            className={`text-xs text-white/60 ${
+              sidebarOpen ? 'block' : 'hidden'
+            }`}
+          >
+            MAIN MENU
+          </h1>
+          <nav className='py-2 px-1'>
+            <ul
+              className={`flex flex-col gap-1 ${
+                sidebarOpen ? '' : 'items-center'
+              }`}
+            >
+              {mainMenuItems.map((item) => (
+                // NAV ITEM
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className={`flex items-center gap-2 rounded-lg p-2 hover:text-white hover:bg-white/15 transition-all duration-100 ${
+                    activeNavItem === item.label
+                      ? 'bg-white/15 text-white'
+                      : 'text-white/75'
+                  }`}
+                  onClick={() => {
+                    setActiveNavItem(item.label);
+                  }}
+                >
+                  <div
+                    className={`${
+                      sidebarOpen ? 'h-4 w-4 ' : 'h-6 w-6'
+                    } flex items-center`}
+                  >
+                    {item.icon}
+                  </div>
+                  <h2
+                    className={`font-light text-sm ${
+                      sidebarOpen ? 'block' : 'hidden'
+                    }`}
+                  >
+                    {item.label}
+                  </h2>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        </section>
 
         {/* INVESTMENTS NAVBAR */}
+        <section className='px-3 pt-5'>
+          <h1
+            className={`text-xs text-white/60 ${
+              sidebarOpen ? 'block' : 'hidden'
+            }`}
+          >
+            INVESTMENTS
+          </h1>
+          <nav className='py-2 px-1'>
+            <ul
+              className={`flex flex-col gap-1 ${
+                sidebarOpen ? '' : 'items-center'
+              }`}
+            >
+              {investmentsMenuItems.map((item) => (
+                // NAV ITEM
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className={`flex items-center gap-2 rounded-lg p-2 hover:text-white hover:bg-white/15 transition-all duration-100 ${
+                    activeNavItem === item.label
+                      ? 'bg-white/15 text-white'
+                      : 'text-white/75'
+                  }`}
+                  onClick={() => {
+                    setActiveNavItem(item.label);
+                  }}
+                >
+                  <div
+                    className={`${
+                      sidebarOpen ? 'h-4 w-4 ' : 'h-6 w-6'
+                    } flex items-center`}
+                  >
+                    {item.icon}
+                  </div>
+                  <h2
+                    className={`font-light text-sm ${
+                      sidebarOpen ? 'block' : 'hidden'
+                    }`}
+                  >
+                    {item.label}
+                  </h2>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        </section>
       </section>
 
       {/* FOOTER */}
-      <footer></footer>
+      <footer className='h-full'>
+        <ul
+          className={`h-full flex flex-col px-3 pb-4 justify-end gap-1 ${
+            sidebarOpen ? '' : 'items-center'
+          }`}
+        >
+          {footerItems.map((item) => (
+            <li key={item.label}>
+              <Link
+                href={item.href}
+                className='flex items-center gap-2 py-2 px-1 text-white/60 hover:text-white hover:cursor-pointer transition-all duration-100'
+              >
+                <div className='h-6 w-6 flex items-center'>{item.icon}</div>
+                <h2
+                  className={`font-light text-sm ${
+                    sidebarOpen ? 'block' : 'hidden'
+                  }`}
+                >
+                  {item.label}
+                </h2>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </footer>
     </aside>
   );
 }
